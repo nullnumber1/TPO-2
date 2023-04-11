d9ed8c751af614eaf6b93ac74b72fa8c877aff2a
Артём Романов <artromworkmail@gmail.com>
2023-04-12 00:51:31 +0300

    Main.java

--- src/main/java/com/nullnumber1/Main.java
+++ src/main/java/com/nullnumber1/Main.java
@@ -1,2 +1,10 @@
-package com.nullnumber1;public class Main {
+package com.nullnumber1;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SystemExpression systemExpression = new SystemExpression();
+        System.out.println(systemExpression.systemSolve(-2, 0.0001));
+    }
 }
