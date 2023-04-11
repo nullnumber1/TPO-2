1bbeb25f427c0a3c18644601e68eb6a5ea332623
Артём Романов <artromworkmail@gmail.com>
2023-04-12 00:50:45 +0300

    Cos implementation

--- src/main/java/com/nullnumber1/trigonometric/Cos.java
+++ src/main/java/com/nullnumber1/trigonometric/Cos.java
@@ -1,2 +1,57 @@
-package com.nullnumber1.trigonometric;public class Cos {
+package com.nullnumber1.trigonometric;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Cos {
+
+    private final Sin sin;
+
+    public Cos(Sin sin) {
+        this.sin = sin;
+    }
+
+    public Cos() {
+        this.sin = new Sin();
+    }
+
+    public double cos(double x, double eps) {
+        double x_init = x;
+        x %= Math.PI * 2;
+        if (Double.POSITIVE_INFINITY == x || Double.NEGATIVE_INFINITY == x) {
+            return Double.NaN;
+        }
+        if (x < -Math.PI) {
+            while (x < -Math.PI) x += 2 * Math.PI;
+        }
+        if (x > Math.PI) {
+            while (x > Math.PI) x -= 2 * Math.PI;
+        }
+        double result;
+        if (x > Math.PI / 2 || x < -Math.PI / 2) {
+            result = -1 * Math.sqrt(1 - sin.sin(x_init, eps) * sin.sin(x_init, eps));
+        } else result = Math.sqrt(1 - sin.sin(x_init, eps) * sin.sin(x_init, eps));
+        if (Math.abs(result) > 1) return Double.NaN;
+        if (Math.abs(result) <= eps) return 0;
+        return result;
+    }
+
+    public void writeResultToCsv(final String filename, final double from, final double to, final double step, final double eps) throws IOException {
+        final Path path = Paths.get(filename);
+        final File file = new File(path.toUri());
+        if (file.exists()) {
+            Files.delete(path);
+        }
+        file.createNewFile();
+
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            for (double current = from; current <= to; current += step) {
+                printWriter.println(current + "," + this.cos(current, eps));
+            }
+        }
+    }
 }
