08edd4354bc851a514a7148149b847c2de134191
Артём Романов <artromworkmail@gmail.com>
2023-04-12 00:49:55 +0300

    Add deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.commons:commons-csv:1.10.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.2.0")
 }
 
 tasks.test {
