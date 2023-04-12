528d58ea636eaff86d616c0b8ad6d67b054d7ba5
Артём Романов <artromworkmail@gmail.com>
2023-04-12 12:19:31 +0300

    Plotter to deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.apache.commons:commons-csv:1.10.0")
+    implementation("org.knowm.xchart:xchart:3.8.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.2.0")
